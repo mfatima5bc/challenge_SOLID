c6ae6552a1174a431bf0b4d68ee76bb979dcf0c1
Maria de Fátima <maria.defatimabarbosacardoso@gmail.com>
2022-06-12 18:16:23 -0300

    docs: Created swagger api documentation

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -12,7 +12,7 @@ class ShowUserProfileController {
 
       const user = this.showUserProfileUseCase.execute({ user_id });
 
-      return response.status(201).json(user);
+      return response.status(200).json(user);
     } catch (error) {
       return response.status(404).json({ error: error.message });
     }
